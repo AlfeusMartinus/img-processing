9d46ab3fbd371ae3e0cbd1708a1bcbd0031699ea
Yannis <ioannis.kalfas@kuleuven.be>
2023-08-08 14:14:26 +0200

    uploading a streamlit app

--- README.md
+++ README.md
@@ -1,31 +1,90 @@
-# Project Name
+# Image Processing Functions
 
-Brief description or tagline for your project.
-
-## About
-
-Briefly describe what your project does and its main features or goals.
+This Streamlit application provides various image processing functions that allow you to alter and visualize images. The application uses Python, OpenCV, and other libraries to perform image processing operations.
 
 ## Getting Started
 
-### Prerequisites
+To run this application locally, follow these steps:
 
-List any prerequisites or dependencies users need to have installed before using your project.
+1. Make sure you have Python installed on your machine.
 
-### Installation
+2. Install the required Python packages using the following command:
 
-Provide step-by-step instructions on how to install your project.
+   ```bash
+   pip install streamlit opencv-python numpy natsort scipy skimage rembg streamlit_image_comparison    
+3. Download the code from this repository or copy the code provided below.
+
+4. Open a terminal window and navigate to the directory where the code is located.
+
+5. Run the Streamlit app using the following command: 
+
+   ```bash
+   streamlit run image_processing_functions.py
+6. The Streamlit app will open in a web browser, allowing you to upload an image and adjust various image processing settings.
+
+## Features
+
+This Streamlit application offers the following features for image processing:
+
+- **Load and Visualize Images:** Upload an image using the file uploader and visualize it in the Streamlit app.
+
+- **Adjust Cropping:** Customize cropping settings for different portions of the image, including the top, bottom, left, and right edges.
+
+- **Resize Images:** Use the provided slider to resize the image according to your preferences.
+
+- **Apply Various Image Processing Operations:** Choose from a range of image processing operations to alter the appearance of the image:
+
+  - **Automatic White Balance (AWB):** Perform automatic white balance correction based on the "Gray World" assumption.
+
+  - **Custom White Balance (AWB_custom):** Apply custom white balance by adjusting factors for each color channel.
+
+  - **Blur:** Apply Gaussian blurring to the image, with adjustable sigma values.
+
+  - **Grayscale:** Convert the image to grayscale and then back to color.
+
+  - **Denoise:** Apply bilateral filtering for denoising, with adjustable parameters.
+
+  - **Contrast:** Adjust the contrast of the image using a slider.
+
+  - **Brightness:** Modify the brightness of the image using a slider.
+
+  - **Gamma Correction:** Apply gamma correction to adjust image contrast and brightness.
+
+  - **Saturation:** Alter the saturation of the image using a slider.
+
+  - **Sharpness:** Enhance image sharpness using convolutional filters and Gaussian blurring.
+
+  - **Contrast Limited Adaptive Histogram Equalization (CLAHE):** Apply adaptive histogram equalization for enhanced contrast.
+
+  - **Background Removal:** Use the "rembg" library to remove the background from the image.
+
+  - **Image Tiling:** Divide the image into smaller tiles, optionally with a grid drawn around each tile.
+
+  - **Sobel Edge Detection (Vertical and Horizontal):** Apply Sobel edge detection in both vertical and horizontal directions.
+
+- **Toggle Before/After Images:** Utilize the "Before/After slider tool" to toggle between the original and processed images.
+
+- **Visualize Processed Images:** Display the processed image in the Streamlit app, either as a standalone image or in comparison mode.
 
 ## Usage
 
-Explain how to use your project. Provide examples and commands if applicable.
+1. **Upload Image:** Use the file uploader to load an image.
 
-## Contributing
+2. **Crop:** Adjust cropping to keep desired parts of the image.
 
-Explain how others can contribute to your project.
+3. **Resize:** Change image size using the slider.
+
+4. **Process:** Select operations and adjust settings as needed.
+
+5. **Apply:** Click "Initialize" to apply settings.
+
+6. **View:** Compare processed image to original using "Before/After" tool.
+
+7. **Experiment:** Try different settings for desired effects.
 
 ## License
 
-Specify the license under which your project is released. For example:
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
 
-This project is licensed under the [MIT License](LICENSE).
+## Contact
+Yannis Kalfas - [kalfasyan](mailto:kalfasyan@gmail.com)
---
+++ img_processing_webgui.py
@@ -0,0 +1,225 @@
+import streamlit as st
+import cv2
+import numpy as np
+from natsort import natsorted
+import os
+from scipy import ndimage
+from skimage import io, feature, transform, filters 
+
+st.set_page_config(page_title="Image-Comparison Example", layout="centered")
+
+st.title("Image Processing Functions")
+
+# Let the user select the image they want to use
+image = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg", "tif", "tiff"])
+if image is None: st.info("Please upload an image file."); st.stop()
+
+image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), 1)
+# Delete any existing image.png file and altered.png file
+try: os.remove("image.png")
+except: pass
+try: os.remove("altered.png")
+except: pass
+
+
+with st.form(key='my_form', clear_on_submit=False):
+    st.subheader("Settings")
+    # Add a toggle button for including the before/after image
+    before_after = st.checkbox("Before/After slider tool", value=True)
+    col1, col2 = st.columns(2)
+    top_crop = col1.number_input("Top Crop %", 0, 100, 0)
+    bottom_crop = col2.number_input("Bottom Crop %", 0, 100, 0)
+    col1, col2 = st.columns(2)
+    left_crop = col1.number_input("Left Crop", 0, 100, 0)
+    right_crop = col2.number_input("Right Crop", 0, 100, 0)
+
+    # Resize the image to a user selected size
+    resize = st.slider('Resize', 0, 100, 100)
+
+    # Give the user some options for altering the image with multiselect
+    img_processing_operations = natsorted(["AWB", "Blur", "Grayscale", 
+                                "Denoise", "Contrast", "Brightness",
+                                "Saturation", "Sharpness", "CLAHE", 
+                                "AWB_custom", "Gamma", "Remove-Background",
+                                "Tile-Plakakia","Sobel_Vertical","Sobel_Horizontal"])
+    options = st.multiselect("Alter the image...", img_processing_operations)
+
+    if "AWB_custom" in options:
+        # Perform custom white balance i.e. the user can select the factor for each channel
+        col1, col2 = st.columns(2)
+        with col1: awb_b = st.slider('AWB B', 0.0, 10.0, 3.0)
+        with col2: awb_g = st.slider('AWB G', 0.0, 10.0, 3.0)
+    if "Blur" in options:
+        # Blur the image using a Gaussian filter and a slider for the sigma value
+        sigmablur = st.slider('Blur Sigma', 0.0, 10.0, 3.0)
+    if "Denoise" in options:
+        col1, col2, col3 = st.columns(3)
+        with col1: diameter = st.slider('Denoise Diameter', 0, 10, 3)
+        with col2: sigmaColor = st.slider('Denoise Sigma Color', 0, 10, 3)
+        with col3: sigmaSpace = st.slider('Denoise Sigma Space', 0, 10, 3)
+    if "Sharpness" in options:
+        sigmaSharpness = st.slider('Sharpness Sigma', 0.0, 10.0, 3.0)
+    if "Brightness" in options:
+        brightness = st.slider('Brightness', 0.0, 10.0, 3.0)
+    if "Gamma" in options:
+        gamma = st.slider('Gamma', 0.0, 10.0, 3.0)
+    if "Saturation" in options:
+        saturation = st.slider('Saturation', 0.0, 10.0, 3.0)
+    if "CLAHE" in options:
+        col1, col2 = st.columns(2)
+        with col1:
+            clipLimit = st.slider('CLAHE Clip Limit', 0.0, 10.0, 3.0)
+        with col2:
+            tileGridSize = st.slider('CLAHE Tile Grid Size', 0, 10, 3)
+    if "Remove-Background" in options:
+        # Let the user select alpha matting foreground and background threshold values
+        col1, col2, col3 = st.columns(3)
+        with col1: alpha_matting = st.checkbox("Alpha Matting")
+        with col2: bg_threshold = st.slider('Background Threshold', 0, 255, 10)
+        with col3: fg_threshold = st.slider('Foreground Threshold', 0, 255, 240)
+    if "Tile-Plakakia" in options:
+        # Let the user select the tile_size and the step_size
+        col1, col2 = st.columns(2)
+        with col1: tile_size = st.slider('Tile Size', 0, 100, 50)
+        with col2: step_size = st.slider('Step Size', 0, 100, 50)
+
+    # Add a submit button
+    submit_button = st.form_submit_button(label='Initialize')
+
+if submit_button:
+    # Crop the image
+    image = image[:, int(image.shape[1] * (left_crop / 100.0)):int(image.shape[1] * (1 - (right_crop / 100.0))), :]
+    # Crop the image
+    image = image[int(image.shape[0] * (top_crop / 100.0)):int(image.shape[0] * (1 - (bottom_crop / 100.0))), :, :]
+    image = cv2.resize(image, (0, 0), fx=(resize / 100.0), fy=(resize / 100.0))
+    cv2.imwrite("image.png", image)
+
+    if "AWB" in options:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        avg_a = np.average(image[:, :, 1])
+        avg_b = np.average(image[:, :, 2])
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                l, a, b = image[x, y, :]
+                # fix for CV correction
+                l *= 100 / 255.0
+                image[x, y, 1] = a - ((avg_a - 128) * (l / 100.0) * 1.1)
+                image[x, y, 2] = b - ((avg_b - 128) * (l / 100.0) * 1.1)
+        image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+        # Explanation of the above code:
+        # We are using the "Gray World" assumption method to perform white balancing
+        # The idea is that in an image the average value for each channel is the same
+        # So we calculate the average channel value and then adjust each channel
+        # based on the average value and the difference from the theoretical average value of 128
+        # We multiply the average value by 100/255 to normalize the value to 0-100
+        # We then multiply the average value by 1.1 to increase the color balance
+        # Finally we convert the image back to BGR from LAB
+        # The above method works well for most images but can sometimes overcorrect
+        # For this reason we have also included a custom white balance option
+        # The custom white balance option allows the user to select the factor for each channel
+        # The custom white balance option is also useful for images that are already well balanced
+        # Resouces:
+        # https://stackoverflow.com/questions/46390779/automatic-white-balancing-with-grayworld-assumption
+        # https://stackoverflow.com/questions/24341114/simple-illumination-correction-in-images-opencv-c
+        # https://stackoverflow.com/questions/36982736/opencv-white-balancing-with-grayworld-assumption
+        # https://stackoverflow.com/questions/3490727/what-are-some-simple-methods-for-balancing-the-colors-of-an-image
+    if "AWB_custom" in options:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        avg_a = np.average(image[:, :, 1])
+        avg_b = np.average(image[:, :, 2])
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                l, a, b = image[x, y, :]
+                # fix for CV correction
+                l *= 100 / 255.0
+                image[x, y, 1] = a - ((avg_a - 128) * (l / 100.0) * awb_b)
+                image[x, y, 2] = b - ((avg_b - 128) * (l / 100.0) * awb_g)
+        image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+        
+    if "Blur" in options:
+        image = cv2.GaussianBlur(image, (0, 0), sigmablur)
+    if "Grayscale" in options:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    if "Denoise" in options:
+        image = cv2.bilateralFilter(image, diameter, sigmaColor, sigmaSpace)
+    if "Contrast" in options:
+        # Change contrast using a slider
+        contrast = st.slider('Contrast', 0.0, 10.0, 3.0)
+        image = cv2.convertScaleAbs(image, alpha=contrast, beta=0)
+    if "Brightness" in options:
+        image = cv2.convertScaleAbs(image, alpha=brightness, beta=0)
+    if "Gamma" in options:
+        # Perform gamma correction with a slider
+        image = np.power(image / 255.0, gamma)
+        image = cv2.convertScaleAbs(image, alpha=255.0)
+    if "Saturation" in options:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image[:, :, 1] = cv2.convertScaleAbs(image[:, :, 1], alpha=saturation, beta=0)
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    if "Sharpness" in options:
+        # sharpen the colors image
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        image = cv2.filter2D(image, -1, kernel)
+        image = cv2.GaussianBlur(image, (0, 0), sigmaSharpness)
+    if "CLAHE" in options:
+        # convert from BGR to LAB color space
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        # split on 3 different channels
+        l, a, b = cv2.split(image)
+        # apply CLAHE to L-channel
+        clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=(tileGridSize, tileGridSize))
+        l = clahe.apply(l)
+        # merge channels
+        image = cv2.merge((l, a, b))
+        # convert from LAB to BGR
+        image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+    if "Remove-Background" in options:
+        # Use the rembg library to remove the background
+        import rembg
+        # use a slider to adjust the alpha matting
+        image = rembg.remove(image, alpha_matting=alpha_matting, alpha_matting_foreground_threshold=fg_threshold, alpha_matting_background_threshold=bg_threshold)
+    if "Tile-Plakakia" in options:
+        from plakakia.utils_tiling import tile_image
+        tiles, coordinates = tile_image(image, tile_size, step_size)
+
+        # Draw a rectangle around each tile and make it smaller by 1 pixel to see the grid
+        tile_size = int(tile_size)
+        step_size = int(step_size)
+        for i in range(len(coordinates)):
+            x1, y1, x2, y2 = coordinates[i]
+            # Make the color the inverse of the average color of the tile
+            inv_color = (255 - int(np.average(tiles[i, :, :, 0])), 255 - int(np.average(tiles[i, :, :, 1])), 255 - int(np.average(tiles[i, :, :, 2])))
+            # Make the rectangle 1 pixel smaller to see the grid
+            cv2.rectangle(image, (x1, y1), (x2-1, y2-1), inv_color, 1)
+        st.write("Image size: ", image.shape)
+    if "Sobel_Vertical" in options:
+        if len(image.shape) == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = filters.sobel_v(image)
+        image = image / np.max(image)
+        image = (image * 255).astype(np.uint8)
+    if "Sobel_Horizontal" in options:
+        if len(image.shape) == 3:        
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = filters.sobel_h(image)
+        image = image / np.max(image)
+        image = (image * 255).astype(np.uint8)
+
+
+    if image is not None:
+        cv2.imwrite("altered.png", image) 
+    else:
+        st.error("Please initialize")    
+
+    if image is not None:
+        if before_after:
+            from streamlit_image_comparison import image_comparison
+            st.header("Before and after")
+            image_comparison(img1="image.png", img2="altered.png") 
+        else:
+            st.header("Processed image")
+            st.image(image, caption=f"", use_column_width=True, channels="BGR")
+
+    else: 
+        st.stop()
